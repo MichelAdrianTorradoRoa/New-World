996f570d3343b4e140afe2258196a6459ae8ca9b
MichelAdrianTorradoRoa <micheltorrado12@gmail.com>
2024-05-10 21:58:00 -0500

    feat.pr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from datos import *
 from menu import *
+from usuarios import *
 
 BASE_DE_DATOS="registro.json"
 
@@ -9,17 +10,56 @@ while True:
     menu_principal()
     opc=pedir_opcion()
     if opc==1:
-        datos=
+        while True:
+            menu_Registro_y_Gestión_de_Usuarios()
+            opc=pedir_opcion()
+            if opc==1:
+                datos=crear_perfiles_usuarios(datos)
+            elif opc == 10:
+                print("Salió!!")
+                break
+    
+        
+            
+    elif opc==2:
+        while True:
+            menu_Seguimiento_del_Historial_de_Usuarios()
+            opc=pedir_opcion()
+            if opc==1:
+                datos=crear_perfiles_usuarios(datos)
+            elif opc == 3:
+                print("Salió!!")
+                break
+    
 
 
 
 
+    elif opc==4:
+        while True:
+            menu_Gestión_de_las_Ventas()
+            opc=pedir_opcion()
+            if opc==1:
+                datos=crear_perfiles_usuarios(datos)
+            elif opc == 3:
+                print("Salió!!")
+                break
+    
 
 
-
-
-
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    elif opc == 6:
+        print("Salió!!")
     break
+
 
 
 
