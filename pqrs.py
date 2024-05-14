6daff28b803eb2b08bdd1fc3d56c45ee6a916cc7
Michel Adrian Torrado Roa <77382674+MichelAdrianTorradoRoa@users.noreply.github.com>
2024-05-13 20:07:15 -0500

    feat

--- pqrs.py
+++ pqrs.py
@@ -4,23 +4,23 @@ def consulta(datos):
     proceso=input("Escriba el proceso que el usuario ha realizado P(peticion), Q(queja), R(reclamo) o S(sugerencia): ")
     if proceso == "P":
         for P in proceso:
+            contador =+ 1
             print (contador, P)
-            contador += 1
         return datos
     elif proceso=="Q":
         for Q in proceso:
+            contador =+ 1
             print (contador, Q)
-            contador += 1
         return datos
     elif proceso=="R":
         for R in proceso:
-            print (contador, R)
             contador += 1
+            print (contador, R)
         return datos
     elif proceso=="S":
         for S in proceso:
-            print (contador, S)
             contador += 1
+            print (contador, S)
         return datos         
 
 
