d39574e7dae3a0cf38273700b64adadfb9b44787
MichelAdrianTorradoRoa <micheltorrado12@gmail.com>
2024-05-21 14:22:25 -0500

    feat

--- menu.py
+++ menu.py
@@ -10,7 +10,7 @@ file.close()
 
 def guardar_txt(dato, mensaje): 
     dato_c = dato + " " + mensaje
-    with open("fallos.txt", "a") as file:
+    with open("errores.txt", "a") as file:
         file.write(dato_c + '\n')
     file.close()
 
